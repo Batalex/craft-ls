e219dcd41d6b6d6570208ee7a76aa61fdb98b759
Alex Batisse <alexandre.batisse@hey.com>
2024-12-03 12:22:47 +0100

    WIP Add work files

--- src/craft_ls/__main__.py
+++ src/craft_ls/__main__.py
@@ -1,3 +1,8 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def main() -> None:
     from craft_ls import server
 
--- src/craft_ls/server.py
+++ src/craft_ls/server.py
@@ -1,6 +1,20 @@
 from pygls import server
+from lsprotocol import types
 
 craft_lserver = server.LanguageServer(name="craft-ls", version="0.1.0")
+
+
+@craft_lserver.feature(types.TEXT_DOCUMENT_COMPLETION)
+def completions(params: types.CompletionParams):
+    items = []
+    document = craft_lserver.workspace.get_text_document(params.text_document.uri)
+    current_line = document.lines[params.position.line].strip()
+    if current_line.endswith("hello."):
+        items = [
+            types.CompletionItem(label="world"),
+            types.CompletionItem(label="friend"),
+        ]
+    return types.CompletionList(is_incomplete=False, items=items)
 
 
 def start() -> None:
