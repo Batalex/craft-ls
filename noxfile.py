618fba74e93b960f8c6484a1d2e70b92d41031f6
Alex Batisse <alex.batisse@canonical.com>
2025-05-06 14:54:14 +0200

    feat: Download and patch jsonschemas in nox task (#6)
    * feat: Add nox task to dl and patch jsonschemas
    
    * fix: Allow required validation error message override

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,22 @@
 """Tasks definition for the nox runner."""
 
+from pathlib import Path
+from urllib.request import urlopen
+
 import nox
 
 nox.options.default_venv_backend = "uv"
 nox.options.reuse_venv = "yes"
 nox.options.sessions = ["fmt", "lint"]
 
+SNAPCRAFT_URL = "https://raw.githubusercontent.com/canonical/snapcraft/refs/heads/main/schema/snapcraft.json"
+ROCKCRAFT_URL = "https://raw.githubusercontent.com/canonical/rockcraft/refs/heads/main/schema/rockcraft.json"
+CHARMCRAFT_URL = "https://raw.githubusercontent.com/canonical/charmcraft/refs/heads/main/schema/charmcraft.json"
+
 
 @nox.session()
 def fmt(session: nox.Session) -> None:
-    """
-    Format source code.
-    """
+    """Format source code."""
     session.run(
         "uv",
         "sync",
@@ -25,9 +30,7 @@ def fmt(session: nox.Session) -> None:
 
 @nox.session()
 def lint(session: nox.Session) -> None:
-    """
-    Lint source code.
-    """
+    """Lint source code."""
     session.run(
         "uv",
         "sync",
@@ -43,9 +46,7 @@ def lint(session: nox.Session) -> None:
 
 @nox.session()
 def tests(session: nox.Session) -> None:
-    """
-    Run the unit tests.
-    """
+    """Run the unit tests."""
     session.run_install(
         "uv",
         "sync",
@@ -55,3 +56,59 @@ def tests(session: nox.Session) -> None:
         env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
     )
     session.run("python", "-m", "pytest", *session.posargs)
+
+
+@nox.session()
+def build_schemas(session=nox.Session) -> None:
+    """Fetch the latest schemas available and apply patches."""
+    tmp_build = Path(".build")
+    resources_build = Path("build")
+    src = Path("src")
+    tmp_build.mkdir(exist_ok=True)
+
+    # Snapcraft
+    session.log("Getting latest snapcraft schema")
+
+    with (
+        (snap := tmp_build / "snapcraft_upstream.json").open(
+            "w", encoding="utf-8"
+        ) as f,
+        urlopen(SNAPCRAFT_URL) as g,
+    ):
+        f.write(g.read().decode("utf-8"))
+
+    for patch_file in sorted(resources_build.glob("snap_*"), reverse=True):
+        session.run("patch", snap, "-i", patch_file, external=True)
+
+    # Rockcraft
+    session.log("Getting latest rockcraft schema")
+
+    with (
+        (rock := tmp_build / "rockcraft_upstream.json").open(
+            "w", encoding="utf-8"
+        ) as f,
+        urlopen(ROCKCRAFT_URL) as g,
+    ):
+        f.write(g.read().decode("utf-8"))
+
+    for patch_file in sorted(resources_build.glob("rock_*"), reverse=True):
+        session.run("patch", rock, "-i", patch_file, external=True)
+
+    # Charmcraft
+    session.log("Getting latest charmcraft schema")
+
+    with (
+        (charm := tmp_build / "charmcraft_upstream.json").open(
+            "w", encoding="utf-8"
+        ) as f,
+        urlopen(ROCKCRAFT_URL) as g,
+    ):
+        f.write(g.read().decode("utf-8"))
+
+    for patch_file in sorted(resources_build.glob("charm_*"), reverse=True):
+        session.run("patch", charm, "-i", patch_file, external=True)
+
+    session.log("Replacing schemas")
+    snap.replace(src / "craft_ls/schemas/snapcraft.json")
+    rock.replace(src / "craft_ls/schemas/rockcraft.json")
+    charm.replace(src / "craft_ls/schemas/charmcraft.json")
