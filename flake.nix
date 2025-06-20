f068d51bef5ffd4691954e6f3a7317d4f0bf8d3c
Alex Batisse <alex.batisse@canonical.com>
2025-06-20 12:23:21 +0200

    chore: Bump version to 0.3.1

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     in {
       packages.default = pythonPkgs.buildPythonPackage {
         pname = "craft-ls";
-        version = "0.3.0";
+        version = "0.3.1";
         format = "pyproject";
         src = ./.;
         build-system = [pythonPkgs.hatchling];
